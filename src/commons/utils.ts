2a92ee16aea9452379ddaef44446d3b31b97d922
Alan D <alan@vmodev.com>
2022-05-26 16:35:15 +0700

    Fix convert time

--- src/commons/utils.ts
+++ src/commons/utils.ts
@@ -1,5 +1,7 @@
 export const convertTime = (time: string) => {
   if (time) {
-    return String(new Date(time)).replace("GMT+0700 (Indochina Time)", "");
+    return String(new Date(time))
+      .replace("GMT+0700 (Indochina Time)", "")
+      .replace("GMT+0700 (Giờ Đông Dương)", "");
   }
 };
